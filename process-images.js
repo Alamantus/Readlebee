42eff58ca793fc8a236f99d64968d98c2c9c2b5b
Robbie Antenesse <dev@alamantus.com>
2019-09-06 18:06:55 -0600

    Add icon and image processor for raw svgs

---
+++ process-images.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const sharp = require('sharp');
+
+const folder = './public/images/';
+
+if (!fs.existsSync('./public')) {
+  fs.mkdirSync('./public');
+}
+if (!fs.existsSync('./public/images')) {
+  fs.mkdirSync('./public/images');
+}
+
+const favicon = sharp('./images/book-pile.svg');
+
+// sharp('./src/images/social.jpg').toFile(folder + 'social.jpg', (err, info) => {
+//   if (err) return console.error(err);
+//   console.log(info);
+// });
+
+favicon.clone().resize(32, 32).toFile(folder + 'favicon.png', (err, info) => {
+  if (err) return console.error(err);
+  console.log(info);
+});
+
+favicon.clone().resize(128, 128).toFile(folder + 'icon-128.png', (err, info) => {
+  if (err) return console.error(err);
+  console.log(info);
+});
+
+favicon.clone().resize(144, 144).toFile(folder + 'icon-144.png', (err, info) => {
+  if (err) return console.error(err);
+  console.log(info);
+});
+
+favicon.clone().resize(152, 152).toFile(folder + 'icon-152.png', (err, info) => {
+  if (err) return console.error(err);
+  console.log(info);
+});
+
+favicon.clone().resize(192, 192).toFile(folder + 'icon-192.png', (err, info) => {
+  if (err) return console.error(err);
+  console.log(info);
+});
+
+favicon.clone().resize(256, 256).toFile(folder + 'icon-256.png', (err, info) => {
+  if (err) return console.error(err);
+  console.log(info);
+});
+
+favicon.clone().resize(512, 512).toFile(folder + 'icon-512.png', (err, info) => {
+  if (err) return console.error(err);
+  console.log(info);
+});
--- routes/resources.js
+++ routes/resources.js
@@ -3,13 +3,55 @@ async function routes(fastify, options) {
     reply.sendFile('css/' + request.params.css);
   });
 
-  // fastify.get('/search/:id', async function (request, reply) {
-  //   const result = await collection.findOne({ id: request.params.id })
-  //   if (result.value === null) {
-  //     throw new Error('Invalid value')
-  //   }
-  //   return result.value
-  // })
+  fastify.get('/images/:image', async (request, reply) => {
+    reply.sendFile('images/' + request.params.image);
+  });
+
+  fastify.get('/manifest.webmanifest', async (request, reply) => {
+    const manifest = {
+      name: typeof fastify.siteConfig !== 'undefined' ? fastify.siteConfig.siteName : 'book-tracker',
+      short_name: typeof fastify.siteConfig !== 'undefined' ? fastify.siteConfig.siteName : 'book-tracker',
+      icons: [
+        {
+          src: '/images/icon-128.png',
+          sizes: '128x128',
+          type: 'image/png',
+        },
+        {
+          src: '/images/icon-144.png',
+          sizes: '144x144',
+          type: 'image/png',
+        },
+        {
+          src: '/images/icon-152.png',
+          sizes: '152x152',
+          type: 'image/png',
+        },
+        {
+          src: '/images/icon-192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: '/images/icon-256.png',
+          sizes: '256x256',
+          type: 'image/png',
+        },
+        {
+          src: '/images/icon-512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        }
+      ],
+      start_url: '/',
+      display: 'standalone',
+      orientation: 'portrait',
+      background_color: '#000000',
+      theme_color: '#1C4AFF',
+    };
+
+    return JSON.stringify(manifest);
+  });
 }
 
 module.exports = routes
