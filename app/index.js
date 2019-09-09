30cd02f680b276c1f15a298b4494a37aad3370fd
Robbie Antenesse <dev@alamantus.com>
2019-09-09 14:22:09 -0600

    Add settings management functions to choo app; Add language to state

--- app/index.js
+++ app/index.js
@@ -10,10 +10,34 @@ if (process.env.NODE_ENV !== 'production') {
   app.use(require('choo-devtools')());  // Exposes `choo` to the console for debugging!
 }
 
+app.use((state, emitter) => {
+  app.getSettingsItem = settingsKey => {
+    let savedSettings = window.localStorage.getItem('settings');
+    if (savedSettings) {
+      savedSettings = JSON.parse(savedSettings);
+      if (typeof savedSettings[settingsKey] !== 'undefined') {
+        return savedSettings[settingsKey];
+      }
+    }
+    return null;
+  }
+  app.setSettingsItem = (settingsKey, value) => {
+    let savedSettings = window.localStorage.getItem('settings');
+    if (savedSettings) {
+      savedSettings = JSON.parse(savedSettings);
+    } else {
+      savedSettings = {};
+    }
+    savedSettings[settingsKey] = value;
+    return window.localStorage.setItem(JSON.stringify(savedSettings));
+  }
+});
+
 // App state and emitters
 app.use((state, emitter) => {
   // Default state variables
   state.currentView = 'home';
+  state.language = app.getSettingsItem('lang') ? app.getSettingsItem('lang') : (navigator.language || navigator.userLanguage);
   state.viewStates = {};
 
   // Listeners
@@ -32,6 +56,12 @@ app.use((state, emitter) => {
     emitter.on('change-view', newView => {
       // Change the view and call render. Makes it easier to call within views.
       state.currentView = newView;
+      emitter.emit('render', () => {});
+    });
+    
+    emitter.on('set-language', newLanguage => {
+      app.setSettingsItem('lang', newLanguage);
+      state.language = newLanguage;
       emitter.emit('render', () => {});
     });
   });
