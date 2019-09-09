951d07e11fde588f607ebeb9529d1eb23c1013f1
Robbie Antenesse <dev@alamantus.com>
2019-09-09 17:08:11 -0600

    Use i18n as example in views

--- app/views/login/index.js
+++ app/views/login/index.js
@@ -1,19 +1,23 @@
 import html from 'choo/html';
 
+import { I18n } from '../../i18n';
+
 export const loginView = (state, emit) => {
+  const i18n = new I18n(state);
+
   return html`<section>
 
   <article class="card">
     <div class="container wide">
       <label>
-        <span>Email</span>
+        <span>${i18n.__('login.email')}</span>
         <input type="email" name="email">
       </label>
       <label>
-        <span>Password</span>
+        <span>${i18n.__('login.password')}</span>
         <input type="password" name="password">
       </label>
-      <input type="submit" value="Log In!">
+      <input type="submit" value="${i18n.__('login.login_button')}">
     </div>
   </article>
   
--- app/views/search/index.js
+++ app/views/search/index.js
@@ -1,23 +1,24 @@
 import html from 'choo/html';
 
-// We'll see if code splitting is worth it in the end or if we should combine everything into `src/index.scss`
+import { I18n } from '../../i18n';
 import { SearchController } from './controller';  // The controller for this view, where processing should happen.
 
 // This is the view function that is exported and used in the view manager.
 export const searchView = (state, emit) => {
+  const i18n = new I18n(state);
   const controller = new SearchController(state);
 
-  if (!controller.state.done && controller.hasQuery) {
-    controller.searchOpenLibrary(state.query.for).then(() => {
-      emit('render');
-    });
-  }
+  // if (!controller.state.done && controller.hasQuery) {
+  //   controller.searchOpenLibrary(state.query.for).then(() => {
+  //     emit('render');
+  //   });
+  // }
 
   // Returning an array in a view allows non-shared parent HTML elements.
   // This one doesn't have the problem right now, but it's good to remember.
   return [
     html`<section>
-      <h2 class="subtitle">An attempt at a viable alternative to Goodreads</h2>
+      <h2 class="subtitle">${i18n.__('search.header')}</h2>
 
       <article>
         ${controller.results.map(result => {
