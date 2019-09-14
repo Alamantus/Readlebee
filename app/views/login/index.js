8721810bc6f32c6fbe79b7e0bd46f753321cda5c
Robbie Antenesse <dev@alamantus.com>
2019-09-13 23:34:58 -0600

    Add create account form to /login view

--- app/views/login/index.js
+++ app/views/login/index.js
@@ -8,19 +8,56 @@ export const loginView = (state, emit) => {
 
   return html`<section>
 
-  <article class="card">
-    <div class="container wide">
-      <label>
-        <span>${i18n.__('login.email')}</span>
-        <input type="email" name="email">
-      </label>
-      <label>
-        <span>${i18n.__('login.password')}</span>
-        <input type="password" name="password">
-      </label>
-      <input type="submit" value="${i18n.__('login.login_button')}">
+    <div class="flex one two-700">
+      <div>
+        <article class="card">
+          <header>
+            <h3>${i18n.__('login.log_in')}</h3>
+          </header>
+          <footer>
+            <label>
+              <span>${i18n.__('login.email')}</span>
+              <input type="email" name="email">
+            </label>
+            <label>
+              <span>${i18n.__('login.password')}</span>
+              <input type="password" name="password">
+            </label>
+            <input type="submit" value="${i18n.__('login.login_button')}">
+          </footer>
+        </article>
+      </div>
+      <div>
+        <article class="card">
+          <header>
+            <h3>${i18n.__('login.create_account')}</h3>
+          </header>
+          <footer>
+            <label>
+              <span>${i18n.__('login.email')}</span>
+              <input type="email" name="new_email">
+            </label>
+            <label>
+              <span>${i18n.__('login.password')}</span>
+              <input type="password" name="new_password">
+            </label>
+            <label>
+              <span>${i18n.__('login.confirm_password')}</span>
+              <input type="password" name="confirm_password">
+            </label>
+            <label>
+              <span>${i18n.__('login.username')}</span>
+              <input type="text" name="new_username">
+            </label>
+            <label>
+              <span>${i18n.__('login.display_name')}</span>
+              <input type="text" name="new_displayname">
+            </label>
+            <input type="submit" class="success" value="${i18n.__('login.create_account_button')}">
+          </footer>
+        </article>
+      </div>
     </div>
-  </article>
-  
-</section>`;
+    
+  </section>`;
 }
