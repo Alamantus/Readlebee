b5ef4a9bb5b3456605346c81f1e1d6baa97d396e
Robbie Antenesse <dev@alamantus.com>
2019-10-28 00:02:45 -0600

    Add an about page view that laods from backend

---
+++ app/views/about.js
@@ -0,0 +1,22 @@
+import html from 'choo/html';
+
+export const aboutView = (state, emit, i18n) => {
+  const content = html`<section class="content"><i class="icon-loading animate-spin"></i></section>`;
+  const community = html`<section class="content"></section>`;
+
+  const promises = [];
+  if (typeof i18n.pages.about === 'undefined' || typeof i18n.pages.community === 'undefined') {
+    promises.push(i18n.fetchLocalePage('about'));
+    promises.push(i18n.fetchLocalePage('community'));
+  } else {
+    content.innerHTML = i18n.pages.about;
+    community.innerHTML = i18n.pages.community;
+  }
+  if (promises.length > 0) {
+    Promise.all(promises).then(fulfilled => emit('render'));
+  }
+  return [
+    content,
+    community,
+  ];
+}
