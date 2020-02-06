0189a5cab019b3d2d7429e6bc15b23611d64bed4
Robbie Antenesse <dev@alamantus.com>
2020-02-06 09:31:47 -0700

    Update BookReference model
    Add locale; change sources to object with keys = source and value = id

--- server/sequelize/models/BookReference.js
+++ server/sequelize/models/BookReference.js
@@ -18,14 +18,20 @@ module.exports = sequelize => sequelize.define('BookReference', {
   sources: {
     type: Sequelize.JSON,
     allowNull: false,
-    defaultValue: [],
-    comment: 'A JSON array with each element being an object with named source <STRING> and source id <STRING>',
+    defaultValue: {},
+    comment: 'A JSON object with each key a source name and value a source id',
   },
   covers: {
     type: Sequelize.JSON,
     allowNull: false,
     defaultValue: [],
     comment: 'A JSON array with each element being an object with image url <STRING> and source id <STRING>',
+  },
+  locale: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    defaultValue: 'en',
+    comment: 'A basic locale string',
   },
 }, {
   timestamps: false,
@@ -39,6 +45,9 @@ module.exports = sequelize => sequelize.define('BookReference', {
     },
     {
       fields: ['description'],
+    },
+    {
+      fields: ['locale'],
     },
   ],
 });
